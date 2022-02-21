729f0bdcdced021db1ad5f4f19dfe108800d303e
Samuel <s.ch4rl3t@gmail.com>
2022-02-21 11:21:03 +0100

    Ptit changement

--- Golf Planets/Assets/Scripts/GPE/EndPointScript.cs
+++ Golf Planets/Assets/Scripts/GPE/EndPointScript.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndPointScript : MonoBehaviour
 {
     public Collider2D ball;
 
-    [System.Obsolete]
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision = ball)
         {
             Debug.Log("restart game");
-            Application.LoadLevel("MainGame");
+            SceneManager.LoadScene("MainGame");
         }
     }
 }
