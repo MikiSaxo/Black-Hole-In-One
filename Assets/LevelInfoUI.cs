8d3020d4a373be515c845f51c5192e594ae4687c
Samuel <s.ch4rl3t@gmail.com>
2022-02-24 14:15:54 +0100

    Selection de niveauxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
    pas encore fini

---
+++ Assets/LevelInfoUI.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInfoUI : MonoBehaviour
+{
+    public GameObject[] yellowStars;
+    public GameObject isUnlockedd;
+    LevelInfos _info;
+
+    public void Initialize(LevelInfos info)
+    {
+        _info = info;
+        Refresh();
+    }
+
+    internal void Refresh()
+    {
+        if (_info.isUnlocked)
+        {
+            Debug.Log("Cadena unlock");
+            isUnlockedd.SetActive(false);
+        }
+
+        for (int i = 0; i < _info.yellowStars; i++)
+        {
+            Debug.Log("Met les étoiles qu'il faut au select");
+            yellowStars[i].SetActive(true);
+        }
+    }
+
+    private void Start()
+    {
+        Refresh();
+    }
+}
